0012210607d9f02e9565836583520c2208456873
임동현 <eastring2343@naver.com>
2026-07-23 14:34:16 +0900

    Delete docversion directory

--- docversion/src/main/java/com/docversion/service/VersionOperationException.java
+++
@@ -1,8 +0,0 @@
-package com.docversion.service;
-
-/** 버전 생성/수정 실패. */
-public class VersionOperationException extends RuntimeException {
-    public VersionOperationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
